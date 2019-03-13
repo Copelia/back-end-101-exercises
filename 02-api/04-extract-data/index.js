5bc766065d9c4880eb2e0dbda7b3a61dca3c45c2
unknown <iamcorallium@gmail.com>
2019-03-12 19:08:12 -0600

    Añadiendo mi url para scrapping

--- 02-api/04-extract-data/index.js
+++ 02-api/04-extract-data/index.js
@@ -8,13 +8,13 @@ const writeFile = promisify(fs.writeFile);
 const filePath = path.resolve(__dirname, 'data', 'books.json');
 const saveJSON = content => writeFile(filePath, JSON.stringify(content));
 
-const BOOK_SECTION_SELECTOR = 'div.s-result-item';
+const BOOK_SECTION_SELECTOR = 'tr';
 // const BOOK_TITLE_SELECTOR = 'h5 > a > span';
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.amazon.com/s?k=javascript&__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_2');
+  await page.goto('https://www.goodreads.com/author/list/3873.Mikhail_Bulgakov?utf8=%E2%9C%93&sort=average_rating');
 
     await page.waitForSelector(BOOK_SECTION_SELECTOR);
 
@@ -22,9 +22,10 @@ const BOOK_SECTION_SELECTOR = 'div.s-result-item';
       BOOK_SECTION_SELECTOR,
       bookSections => bookSections.map(
           section => ({
-              title: section.querySelector('h5 > a > span').innerText,
-              image: section.querySelector('img').src,
-              author: section.querySelector('.a-row a.a-size-base').innerText,
+              title: section.querySelector('td > a > span').innerText,
+              image: section.querySelector('td > a > img').src
+              // image: section.querySelector('img').src,
+              // author: section.querySelector('.a-row a.a-size-base').innerText,
           }),
       ),
   );
